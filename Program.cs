d19aaa95be3430c519c8454f83d60da45a4ec1ca
Threads-Creator <thiago2.bsp14@gmail.com>
2025-07-27 10:54:32 -0300

    FEATURE: realizando o desafio e completando o codigo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+var nokia = new Nokia(numero: "12345668", modelo: "xyz", imei: "11122233", memoria: 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Calculadora");
+nokia.ExigirInformacoes();
+
+Console.WriteLine();
+
+var iphone = new Iphone(numero: "35445689", modelo: "zzz", imei: "55555777", memoria: 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Calculadora");
+iphone.ExigirInformacoes();
